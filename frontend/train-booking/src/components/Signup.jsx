ba5eda4a75d408b137518b34cff27ec4dbf8b44b
shahnwajalam10 <91122255+shahnwajalam10@users.noreply.github.com>
2025-04-30 20:37:26 +0530

    Update Signup.jsx

--- frontend/train-booking/src/components/Signup.jsx
+++ frontend/train-booking/src/components/Signup.jsx
@@ -79,7 +79,7 @@ const SignUp = () => {
     setIsLoading(true);
     
     try {
-      const response = await fetch('http://localhost:5000/api/auth/signup', {
+      const response = await fetch('https://train-seat-booking-app-9n8b.onrender.com/api/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
