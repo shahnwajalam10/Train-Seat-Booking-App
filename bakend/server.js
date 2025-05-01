44fbcf85ef248e09e36f769194445a1bb3ef7612
shahnwajalam10 <91122255+shahnwajalam10@users.noreply.github.com>
2025-05-01 12:00:08 +0530

    Update server.js

--- bakend/server.js
+++ bakend/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://train-seat-booking-app-ebl5.vercel.app/",
   credentials: true
 }));
 
